8a5b4e36355adec4698daa2b487949067add06ab
chinthaparthichethan <chethan98chethan@gmail.com>
2025-05-19 10:59:26 +0530

    fixed

--- app.py
+++ app.py
@@ -34,5 +34,8 @@ def run_code():
 
     return jsonify({'output': output})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
